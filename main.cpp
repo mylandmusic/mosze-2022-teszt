b2dc1d2f7e3bb4f915577ce21404fae1f7957947
mylandmusic <71819845+mylandmusic@users.noreply.github.com>
2022-09-12 20:41:57 +0200

    hibak dokumentalasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //int* b, N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //nincs dupla ' és nincs ;
+    for (int i = 0;) //for ciklus hiányos
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //Változót javítani
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //for ciklus közepe hiányos
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //az igazi érték nem szerepel, nincs lezárva
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //későbbi osztás miatt double és kezdőérték megadása
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //; hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
